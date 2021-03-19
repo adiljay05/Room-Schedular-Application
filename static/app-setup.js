c87dbb0bdb8c59d1b2b35954a9a125b015db9a63
Jawad Adil <jawad.adil@student.griffith.ie>
2021-03-19 20:00:00 +0000

    Initiallized git repository

---
+++ static/app-setup.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyClOudNRrnUm4qM2ML3E2XxN5Z59A6UEos",
+  authDomain: "jawad-adil-project1.firebaseapp.com",
+  databaseURL: "https://jawad-adil-project1-default-rtdb.firebaseio.com",
+  projectId: "jawad-adil-project1",
+  storageBucket: "jawad-adil-project1.appspot.com",
+  messagingSenderId: "867070443202",
+  appId: "1:867070443202:web:c6cbb277ced4b7430ac55b",
+  measurementId: "G-PMC80HP3TH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
